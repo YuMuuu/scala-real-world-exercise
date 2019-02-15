a6e381d594318c78d2867cb5781e974af4e2a050
YuMuuu <c01150788f@edu.teu.ac.jp>
2019-02-15 11:05:50 +0900

    collectだけわからなくてググった

--- src/main/scala/exercise1/OptionOperation.scala
+++ src/main/scala/exercise1/OptionOperation.scala
@@ -13,16 +13,48 @@ trait OptionOperation {
 }
 
 class YourImpl extends OptionOperation {
-  def optPlus(a: Option[Int], b: Option[Int]): Option[Int] = ???
-  def optPlusGtTen(a: Option[Int], b: Option[Int]): Option[Int] = ???
+  def optPlus(a: Option[Int], b: Option[Int]): Option[Int] = {
+    for {
+      _a <- a
+      _b <- b
+    }yield _a + _b
+  }
+
+  def optPlusGtTen(a: Option[Int], b: Option[Int]): Option[Int] = optPlus(a, b).filter(_ >= 10)
+
   def optPlusGeTenAsTwiceString(a: Option[Int],
-                                b: Option[Int]): Option[String] = ???
-  def optDiv(a: Option[Int], b: Option[Int]): Option[Int] = ???
+                                b: Option[Int]): Option[String] = {
+    optPlus(a, b) collect {
+      case v if v >= 10 => (v * 2).toString
+      case v            => v.toString
+    }
+  }
+  def optDiv(a: Option[Int], b: Option[Int]): Option[Int] = {
+    for {
+      _a <- a
+      _b <- b
+      if _b != 0
+    }yield _a / _b
+  }
+
   def optFunc(a: Option[Int], b: Option[Int])(
-      f: (Int, Int) => Int): Option[Int] = ???
+      f: (Int, Int) => Int): Option[Int] = {
+    for {
+      _a <- a
+      _b <- b
+    } yield f(_a , _b)
+  }
+
   def optFunc2(a: Option[Double], b: Option[String])(
       f1: Double => Option[Int],
       f2: String => Option[Int],
-      op: (Int, Int) => Int): Option[Int] = ???
+      op: (Int, Int) => Int): Option[Int] = {
+    for {
+      _a <- a
+      ai <- f1(_a)
+      _b <- b
+      bi <- f2(_b)
+    } yield op(ai, bi)
+  }
 
 }
